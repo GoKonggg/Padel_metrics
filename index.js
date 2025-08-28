54ef5e5ae5d5b45cf1658a4d3a82b8c36b19f476
GoKonggg <wiselyjansonhalim90@gmail.com>
2025-08-28 11:46:19 +0700

    update

--- index.js
+++ index.js
@@ -171,49 +171,49 @@ backToAnalysisBtn.addEventListener('click', () => {
 // =========================================================================
 const mockAnalysisData = [
     // Poin 1 (dimulai dari detik ke-12)
-    { time: 12, type: 'ERROR' },   // Pukulan pertama, error. (0:12)
+    { time: 203, type: 'ERROR' },   // Pukulan pertama, error. (0:12)
 
     // Poin 2 (dimulai dari detik ke-25, ada reli singkat)
-    { time: 31, type: 'WINNER' },  // Reli selesai dengan winner. (0:31)
+    { time: 250, type: 'WINNER' },  // Reli selesai dengan winner. (0:31)
 
     // Poin 3 (dimulai dari detik ke-45)
-    { time: 52, type: 'WINNER' },  // Winner cepat. (0:52)
+    { time: 300, type: 'WINNER' },  // Winner cepat. (0:52)
 
     // Poin 4 (dimulai dari 1 menit 5 detik)
-    { time: 68, type: 'ERROR' },   // Error setelah beberapa pukulan. (1:08)
+    { time: 350, type: 'ERROR' },   // Error setelah beberapa pukulan. (1:08)
     
     // Poin 5 (Reli panjang, dimulai dari 1 menit 20 detik)
-    { time: 143, type: 'WINNER' }, // Winner di menit 2:23 (143 detik)
+    { time: 400, type: 'WINNER' }, // Winner di menit 2:23 (143 detik)
     
     // Poin 6 
-    { time: 155, type: 'ERROR' },  // Error di menit 2:35 (155 detik)
+    { time: 420, type: 'ERROR' },  // Error di menit 2:35 (155 detik)
     
     // Poin 7
-    { time: 181, type: 'WINNER' }, // Winner di menit 3:01 (181 detik)
+    { time: 500, type: 'WINNER' }, // Winner di menit 3:01 (181 detik)
     
     // Poin 8
-    { time: 204, type: 'WINNER' }, // Winner di menit 3:24 (204 detik)
+    { time: 532, type: 'WINNER' }, // Winner di menit 3:24 (204 detik)
 
     // Poin 9
-    { time: 220, type: 'ERROR' },  // Error di menit 3:40 (220 detik)
+    { time: 580, type: 'ERROR' },  // Error di menit 3:40 (220 detik)
     
     // Poin 10
-    { time: 245, type: 'WINNER' }, // Winner di menit 4:05 (245 detik)
+    { time: 652, type: 'WINNER' }, // Winner di menit 4:05 (245 detik)
     
     // Poin 11
-    { time: 261, type: 'WINNER' }, // Winner di menit 4:21 (261 detik)
+    { time: 852, type: 'WINNER' }, // Winner di menit 4:21 (261 detik)
     
     // Poin 12
-    { time: 288, type: 'ERROR' },  // Error di menit 4:48 (288 detik)
+    { time: 898, type: 'ERROR' },  // Error di menit 4:48 (288 detik)
     
     // Poin 13
-    { time: 305, type: 'WINNER' }, // Winner di menit 5:05 (305 detik)
+    { time: 952, type: 'WINNER' }, // Winner di menit 5:05 (305 detik)
     
     // Poin 14
-    { time: 318, type: 'ERROR' },  // Error di menit 5:18 (318 detik)
+    { time: 1023, type: 'ERROR' },  // Error di menit 5:18 (318 detik)
 
     // Poin 15
-    { time: 340, type: 'WINNER' }, // Winner di menit 5:40 (340 detik)
+    { time: 1100, type: 'WINNER' }, // Winner di menit 5:40 (340 detik)
 ];
 
 
